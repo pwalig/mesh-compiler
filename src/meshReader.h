b50e7ca0d615479ded4f767f5049b393c8a9b6aa
Paweł <pwalig03@gmail.com>
2024-07-10 16:01:33 +0200

    define allocation limit

--- src/meshReader.h
+++ src/meshReader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <fstream>
 #include <vector>
+#define allocation_limit 10485760 // 10 MB
 
 namespace mesh_reader {
 	template <typename T, typename U>
@@ -11,7 +12,7 @@ namespace mesh_reader {
 }
 
 template<typename T, typename U>
-void mesh_reader::readBuffer(std::ifstream& file, std::vector<T>& buffer)
+inline void mesh_reader::readBuffer(std::ifstream& file, std::vector<T>& buffer)
 {
 	U count;
 	file.read((char*)&count, sizeof(U));
@@ -19,8 +20,17 @@ void mesh_reader::readBuffer(std::ifstream& file, std::vector<T>& buffer)
 }
 
 template<typename T, typename U>
-void mesh_reader::readBuffer(std::ifstream& file, std::vector<T>& buffer, const U& count)
+inline void mesh_reader::readBuffer(std::ifstream& file, std::vector<T>& buffer, const U& count)
 {
+#ifdef allocation_limit
+	if (count * sizeof(T) > allocation_limit)
+		throw std::runtime_error("allocation limit exceeded");
+#endif // allocation_limit
+
 	buffer.resize(count);
 	file.read((char*)(buffer.data()), count * sizeof(T));
 }
+
+#ifdef allocation_limit
+#undef allocation_limit
+#endif // allocation_limit
