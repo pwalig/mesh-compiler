5eb2109a7aa9f8b5f29f1156663bd361aa4cc6fb
Paweł <pwalig03@gmail.com>
2024-12-25 18:46:28 +0100

    Add cfield

---
+++ src/core/fields/cfield.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "sfield.h"
+
+namespace mc {
+	template<typename T>
+	class cfield : public sfield<T> {
+	public:
+		const T value;
+
+		std::vector<T> evaluate(const Inode* node) const override;
+
+		oop_ptr_template_child_define(field, vfield)
+	};
+
+	template<typename T>
+	inline std::vector<T> cfield<T>::evaluate(const Inode* node) const
+	{
+		return std::vector<T>(1, value);
+	}
+}
